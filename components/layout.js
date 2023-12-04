e6810b205635c943459fab1b1046234f4ac0cf0b
Manas <manasm.dev@gmail.com>
2023-12-04 12:27:10 +1000

    updated

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,138 @@
 import { Content } from "../components/Content.js";
+import {
+  Navbar,
+  Link,
+  Text,
+  Avatar,
+  Dropdown,
+  Container,
+} from "@nextui-org/react";
 import { Box } from "../components/Box.js";
 
-export const Layout = ({ children }) => (
-  <Box
-    css={{
-      maxW: "100%",
-    }}
-  >
-    {children}
-    <Content />
-  </Box>
-);
+const collapseItems = [
+  "Profile",
+  "Dashboard",
+  "Activity",
+  "Analytics",
+  "System",
+  "Deployments",
+  "My Settings",
+  "Team Settings",
+  "Help & Feedback",
+  "Log Out",
+];
+
+const PageHeader = () => {
+  return <h3>This is Header</h3>;
+};
+
+export default function Layout({ children }) {
+  return (
+    <Box
+      css={{
+        maxW: "100%",
+      }}
+    >
+      <Navbar isBordered variant="sticky">
+        <Navbar.Toggle showIn="xs" />
+        <Navbar.Brand
+          css={{
+            "@xs": {
+              w: "12%",
+            },
+          }}
+        >
+          <Text b color="inherit" size="$2xl" hideIn="xs">
+            ManasCodes
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content
+          enableCursorHighlight
+          hideIn="xs"
+          activeColor="secondary"
+          variant="highlight"
+        >
+          <Navbar.Link href="/">Home</Navbar.Link>
+          <Navbar.Link href="#">Blogs</Navbar.Link>
+          <Navbar.Link href="#">Projects</Navbar.Link>
+          <Navbar.Link href="/about">About</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content
+          css={{
+            "@xsMax": {
+              w: "100%",
+              jc: "space-between",
+            },
+          }}
+        >
+          <Dropdown placement="bottom-right">
+            <Navbar.Item>
+              <Dropdown.Trigger>
+                <Avatar
+                  bordered
+                  as="button"
+                  color="secondary"
+                  size="md"
+                  src="/images/profile.jpg"
+                />
+              </Dropdown.Trigger>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="User menu actions"
+              color="secondary"
+              onAction={(actionKey) => console.log({ actionKey })}
+            >
+              <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  Signed in as
+                </Text>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  zoey@example.com
+                </Text>
+              </Dropdown.Item>
+              <Dropdown.Item key="settings" withDivider>
+                My Settings
+              </Dropdown.Item>
+              <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+              <Dropdown.Item key="analytics" withDivider>
+                Analytics
+              </Dropdown.Item>
+              <Dropdown.Item key="system">System</Dropdown.Item>
+              <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+              <Dropdown.Item key="help_and_feedback" withDivider>
+                Help & Feedback
+              </Dropdown.Item>
+              <Dropdown.Item key="logout" withDivider color="error">
+                Log Out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Content>
+        <Navbar.Collapse disableAnimation>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem
+              key={item}
+              activeColor="warning"
+              css={{
+                color: index === collapseItems.length - 1 ? "$error" : "",
+              }}
+              isActive={index === 2}
+            >
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
+      </Navbar>
+      <PageHeader />
+      <Container sm>{children}</Container>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
-import { Layout } from "../components/Layout.js";
+import { Text, Spacer } from "@nextui-org/react";
+import Layout from "../components/layout";
+import { Box } from "../components/Box.js";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import Head from "next/head";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   const collapseItems = [
     "Profile",
     "Dashboard",
@@ -16,105 +31,43 @@ export default function Home() {
   ];
 
   return (
-    <Layout>
-      <Navbar isBordered variant="sticky">
-        <Navbar.Toggle showIn="xs" />
-        <Navbar.Brand
+    <Layout home>
+      <Head>
+        <title>Manas Mallick</title>
+      </Head>
+      <Box css={{ px: "$12", mt: "$8", "@xsMax": { px: "$10" } }}>
+        <Text
+          h1
+          size={60}
           css={{
-            "@xs": {
-              w: "12%",
-            },
+            textGradient: "45deg, $blue600 -20%, $pink600 50%",
           }}
+          weight="bold"
         >
-          <Text b color="inherit" size="$2xl" hideIn="xs">
-            ManasCodes
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content
-          enableCursorHighlight
-          hideIn="xs"
-          activeColor="secondary"
-          variant="highlight"
-        >
-          <Navbar.Link href="/">Home</Navbar.Link>
-          <Navbar.Link href="#">Blogs</Navbar.Link>
-          <Navbar.Link href="#">Projects</Navbar.Link>
-          <Navbar.Link href="#">About</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content
-          css={{
-            "@xsMax": {
-              w: "100%",
-              jc: "space-between",
-            },
-          }}
-        >
-          <Dropdown placement="bottom-right">
-            <Navbar.Item>
-              <Dropdown.Trigger>
-                <Avatar
-                  bordered
-                  as="button"
-                  color="secondary"
-                  size="md"
-                  src="/images/profile.jpg"
-                />
-              </Dropdown.Trigger>
-            </Navbar.Item>
-            <Dropdown.Menu
-              aria-label="User menu actions"
-              color="secondary"
-              onAction={(actionKey) => console.log({ actionKey })}
-            >
-              <Dropdown.Item key="profile" css={{ height: "$18" }}>
-                <Text b color="inherit" css={{ d: "flex" }}>
-                  Signed in as
-                </Text>
-                <Text b color="inherit" css={{ d: "flex" }}>
-                  zoey@example.com
-                </Text>
-              </Dropdown.Item>
-              <Dropdown.Item key="settings" withDivider>
-                My Settings
-              </Dropdown.Item>
-              <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
-              <Dropdown.Item key="analytics" withDivider>
-                Analytics
-              </Dropdown.Item>
-              <Dropdown.Item key="system">System</Dropdown.Item>
-              <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
-              <Dropdown.Item key="help_and_feedback" withDivider>
-                Help & Feedback
-              </Dropdown.Item>
-              <Dropdown.Item key="logout" withDivider color="error">
-                Log Out
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Navbar.Content>
-        <Navbar.Collapse disableAnimation>
-          {collapseItems.map((item, index) => (
-            <Navbar.CollapseItem
-              key={item}
-              activeColor="warning"
-              css={{
-                color: index === collapseItems.length - 1 ? "$error" : "",
-              }}
-              isActive={index === 2}
-            >
-              <Link
-                color="inherit"
-                css={{
-                  minWidth: "100%",
-                }}
-                href="#"
-              >
-                {item}
-              </Link>
-            </Navbar.CollapseItem>
-          ))}
-        </Navbar.Collapse>
-      </Navbar>
+          Manas Mallick
+        </Text>
+        <section className={utilStyles.headingMd}>
+          <p>
+            Hello, I'm Manas. I'm a Solutions Architect and Technology
+            Consultant.
+          </p>
+        </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <Spacer y={1} />
+      </Box>
     </Layout>
   );
 }
